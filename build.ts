29dd643fccdd8c9f2a157e0962f9a04ccc02e774
oliverjam <oliverjamesphillips@gmail.com>
2024-06-13 23:33:05 +0100

    Parallelise build script

--- build.ts
+++ build.ts
@@ -1,36 +1,35 @@
 import { readdir } from "node:fs/promises";
-import { join } from "node:path";
+import { join as j } from "node:path";
 
 try {
-	let config = {
-		indir: join("src", "pages"),
-		outdir: "_site",
-		assetdir: join("src", "assets"),
-	};
+	let out = "_site";
+	let pages_in = j("src", "pages");
 
-	await Bun.$`rm -rf ${config.outdir}`;
-	await Bun.$`mkdir -p ${config.outdir}`;
+	// recreate output directory
+	await Bun.$`rm -rf ${out}`;
+	await Bun.$`mkdir -p ${out}`;
 
-	let files = await readdir(config.indir, {
-		recursive: true,
-		withFileTypes: true,
-	});
+	let fs = await readdir(pages_in, { recursive: true, withFileTypes: true });
+	let writers = [];
 
-	for (let f of files) {
+	for (let f of fs) {
 		if (!f.isFile()) continue;
 		if (!f.name.includes(".tsx")) continue;
-		let p = join(import.meta.dir, f.path, f.name);
-		let { default: fn } = await import(p);
-		let html = fn();
-		let outpath = join(
-			f.path.replace(config.indir, config.outdir),
+
+		let in_path = j(import.meta.dir, f.path, f.name);
+		let html = await import(in_path).then((m) => m.default());
+		let out_path = j(
+			f.path.replace(pages_in, out),
 			f.name.replace(".tsx", ".html")
 		);
-		await Bun.write(outpath, html);
+		writers.push(Bun.write(out_path, html));
 	}
 
-	await Bun.$`cp -r ${config.assetdir} ${join(config.outdir, "assets")}`;
+	// copy over assets
+	writers.push(Bun.$`cp -r ${j("src", "assets")} ${j(out, "assets")}`);
 
+	// wait for everything to finish
+	await Promise.all(writers);
 	console.log("⚡ Site built");
 } catch (e) {
 	console.error(e);
