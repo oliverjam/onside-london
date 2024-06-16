bd2a693acb4adde167a966d36087c172114f0b4d
oliverjam <oliverjamesphillips@gmail.com>
2024-06-16 15:44:38 +0100

    Simplify build script even more

--- build.ts
+++ build.ts
@@ -1,36 +1,15 @@
 import { readdir } from "node:fs/promises";
-import { join as j } from "node:path";
 
-try {
-	// recreate output directory
-	await Bun.$`rm -rf _site`;
-	await Bun.$`mkdir -p _site`;
+await Bun.$`rm -rf _site`;
+await Bun.$`mkdir -p _site`;
 
-	let fs = await readdir("src/pages", { recursive: true, withFileTypes: true });
-	let writers = [];
-
-	for (let f of fs) {
-		if (!f.isFile()) continue;
-		if (!f.name.includes(".tsx")) continue;
-
-		let in_path = j(import.meta.dir, f.path, f.name);
-		let html = await import(in_path).then((m) => m.default());
-		let out_path = j(
-			f.path.replace("src/pages", "_site"),
-			f.name.replace(".tsx", ".html")
-		);
-		writers.push(Bun.write(out_path, "<!doctype html>" + html));
-	}
-
-	// copy over assets
-	writers.push(Bun.$`cp -r src/assets _site/assets`);
-
-	// wait for everything to finish
-	await Promise.all(writers);
-	// generate CSS
-	await Bun.$`bunx tailwindcss --minify -i src/main.css -o _site/assets/main.css`;
-	console.log("⚡ Site built");
-} catch (e) {
-	console.error(e);
-	process.exit(1);
+for (let p of await readdir("src/pages", { recursive: true })) {
+	if (!p.endsWith(".tsx")) continue;
+	let out = "_site/" + p.replace(".tsx", ".html");
+	let page = await import("./src/pages/" + p);
+	let html = "<!doctype html>" + page.default();
+	await Bun.write(out, html);
 }
+
+await Bun.$`cp -r src/assets _site/assets`;
+await Bun.$`bunx tailwindcss --minify -i src/main.css -o _site/assets/main.css`.quiet();
